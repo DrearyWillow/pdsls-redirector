ce417ca5b2c81f23e7b7c593a15c76f4365e13cf
MellowKyler <dewerdk@gmail.com>
2024-11-09 13:37:59 -0600

    memery of the highest order

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ let settings = {};
 async function loadSettings() {
   try {
     // Remember to update this if you add more settings
-    const data = await browser.storage.sync.get(['alwaysOpen', 'openInNewTab', 'jsonMode', 'keybinding']);
+    const data = await browser.storage.sync.get();
     console.log('Current settings:', data);
     settings = data || {}
   } catch (error) {
@@ -14,10 +14,8 @@ loadSettings();
 browser.storage.onChanged.addListener((changes, area) => {
   if (area != 'sync') return
   for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
-    if (['alwaysOpen', 'openInNewTab', 'jsonMode', 'keybinding'].includes(key)) {
-      console.log(`Storage key "${key}" changed from`, oldValue, 'to', newValue);
-      settings[key] = newValue;
-    }
+    console.log(`Storage key "${key}" changed from`, oldValue, 'to', newValue);
+    settings[key] = newValue;
   }
 });
 
@@ -29,6 +27,17 @@ browser.contextMenus.create({
     contexts: ["all"]
 });
 
+
+const getDid = async (handle) => {
+  if (handle.startsWith("did:")) return handle;
+  try {
+    did = await resolveHandle(handle);
+  } catch (err) {
+    return "";
+  }
+  return did
+}
+
 const resolveHandle = async (handle) => {
   const res = await fetch(
     `https://public.api.bsky.app/xrpc/com.atproto.identity.resolveHandle?handle=` +
@@ -37,8 +46,23 @@ const resolveHandle = async (handle) => {
   return res.json().then((json) => json.did);
 };
 
-// getPostThread
-// const url = `https://public.api.bsky.app/xrpc/app.bsky.feed.getPostThread?uri=${uri}/app.bsky.feed.post/${rkey}&depth=0&parent-height=0`
+const getWhiteWindServiceEndpoint = async () => {
+  if ("WORKAROUND") {
+    return `https://b481a144-23e5-4fa9-83ae-e448d3593655.whtwnd.com`
+  }
+  const res = await fetch(
+    `https://whtwnd.com/.well-known/did.json`
+  );
+  return res.json().then((json) => json.service[0].serviceEndpoint)
+}
+
+const getWhiteWindUri = async (did, title) => {
+  let se = await getWhiteWindServiceEndpoint()
+  const res = await fetch(
+    `${se}/xrpc/com.whtwnd.blog.getEntryMetadataByName?author=${did}&entryTitle=${title}`
+  );
+  return res.json().then((json) => json.entryUri)
+}
 
 // Validate and format URL
 async function validateUrl(url) {
@@ -48,31 +72,52 @@ async function validateUrl(url) {
     if (
       !url.startsWith("https://bsky.app/") &&
       !url.startsWith("https://main.bsky.dev/") &&
+      !url.startsWith("https://whtwnd.com/") &&
       url.startsWith("https://")
     ) return `/${url.replace("https://", "").replace("/", "")}`;
 
-    // URI
-    const uri = url
-      .replace("at://", "")
-      .replace("https://bsky.app/profile/", "")
-      .replace("https://main.bsky.dev/profile/", "")
-      .replace("/post/", "/app.bsky.feed.post/");
-    let did = "";
-    try {
-      if (uri.startsWith("did:")) did = uri.split("/")[0];
-      else did = await resolveHandle(uri.split("/")[0]);
-      if (!did) return "";
-    } catch (err) {
-      return "";
+    if (url.startsWith("https://whtwnd.com/")) {
+      let uri = url.replace("https://whtwnd.com/", "")
+      if (uri.includes("/entries/")) {
+        uri = uri.replace("/entries/", "/com.whtwnd.blog.entry/");
+        [handle = "", lexicon = "", title = ""] = uri.split("/");
+      } else {
+        [handle = "", title = ""] = uri.split("/");
+        lexicon = `com.whtwnd.blog.entry`
+      }
+      
+      let did = await getDid(handle)
+      
+      if (!title) return `https://pdsls.dev/at/${did}`
+      // if title is rkey
+      if (!title.includes("%20")) return `https://pdsls.dev/at/${did}/${lexicon}/${title}`;
+      // uri = (await getWhiteWindUri(did, title)).replace("at://", "")
+      // hard return whtwnd entry collection for now, because their api is mean :(
+      return `https://pdsls.dev/at/${did}/${lexicon}`
+
+    } else {
+      let uri = url
+        .replace("https://bsky.app/profile/", "")
+        .replace("https://main.bsky.dev/profile/", "")
+        .replace("/post/", "/app.bsky.feed.post/")
+      let parts = uri.split("/")
+      let did = await getDid(parts[0])
+
+      if (!did) return ""
+
+      uri = `${did}${parts.length > 1 ? "/" + parts.slice(1).join("/") : ""}`
+      if ((parts[2] === "app.bsky.feed.post") && settings.jsonMode) {
+        // TODO: depth and parent-height settings
+        return `https://public.api.bsky.app/xrpc/app.bsky.feed.getPostThread?uri=${uri}&depth=0&parent-height=0`
+      }
     }
 
-    return `at/${did}${uri.split("/").length > 1 ? "/" + uri.split("/").slice(1).join("/") : ""}`
+    return `https://pdsls.dev/at/${uri}`
 }
 
 // Function to open a new tab with the current page URL
 async function openNewTab(url) {
   console.log("Reached: openNewTab")
-  const ROOT = "https://pdsls.dev"
 
   if (!url) {
     const tabs = await browser.tabs.query({ active: true, currentWindow: true });
@@ -80,15 +125,19 @@ async function openNewTab(url) {
   }
   if (!url) { console.log("Error: No URL"); return; }
 
-  let suffix = await validateUrl(url);
-  if (!suffix && !settings.alwaysOpen) return;
+  let newUrl = await validateUrl(url);
+  if (!newUrl) {
+    if (settings.alwaysOpen) {
+      newUrl = `https://pdsls.dev`;
+    } else return;
+  }
   if (settings.openInNewTab) {
-    await browser.tabs.create({ url: `${ROOT}/${suffix || ""}` });
+    await browser.tabs.create({ url: newUrl });
   } else {
-    await browser.tabs.update({ url: `${ROOT}/${suffix || ""}` });
+    await browser.tabs.update({ url: newUrl });
   }
      
-  console.log(`URL opened: ${ROOT}/${suffix || ""}`);
+  console.log(`URL opened: ${newUrl}`);
 }
 
 // Extension Icon
